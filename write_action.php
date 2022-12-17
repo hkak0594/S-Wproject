a2a5b69be4c5ff2c64080f0369116ded6647ee44
hkak0594 <ghznfl4@naver.com>
2022-12-17 16:40:21 +0900

    write_action

---
+++ write_action.php
@@ -0,0 +1,30 @@
+<?php
+$connect = mysqli_connect("118.67.134.160", "root", "zoha!@#123", "test") or die("fail");
+
+
+$id = $_POST['name'];                   //Writer
+$title = $_POST['title'];               //Title
+$content = $_POST['content'];           //Content
+$date = date('Y-m-d H:i:s');            //Date
+
+$URL = './board.php';                   //return URL
+
+
+$query = "INSERT INTO board (number, title, content, date, hit, id) 
+        values(null,'$title', '$content', '$date', 0, '$id')";
+
+
+$result = $connect->query($query);
+if ($result) {
+?> <script>
+        alert("<?php echo "게시글이 등록되었습니다." ?>");
+        location.replace("<?php echo $URL ?>");
+    </script>
+<?php
+} else {
+    echo "게시글 등록에 실패하였습니다.";
+}
+
+mysqli_close($connect);
+?>
+ 
